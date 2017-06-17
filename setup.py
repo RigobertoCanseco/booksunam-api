14d7bc3a4d4e8a8282168b9d4b303b1305e3e3ea
rigobertocanseco <cansecorigoberto@gmail.com>
2017-06-16 23:47:04 -0500

    change README

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 
-setup(name='FlaskApp',
+
+setup(name='Books UNAM API',
       version='1.0',
-      description='A basic Flask app with static files',
-      author='Ryan Jarvinen',
-      author_email='ryanj@redhat.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-     install_requires=['Flask>=0.10.1'],
+      description='API for find books of the National Autonomous University of Mexico (UNAM)',
+      author='Rigoberto Canseco',
+      author_email='rigobertocanseco@gmail.com',
+      url='',
+      install_requires=['Flask>=0.10.1'],
      )
