78b6c3b9c37dc13f4acb56b7a3f266f1848292e6
rigobertocanseco <cansecorigoberto@gmail.com>
2017-06-17 17:01:54 -0500

    testing flaskapp

--- v1/__init__.py
+++
@@ -1,8 +0,0 @@
-# from flask_sqlalchemy import SQLAlchemy
-# from flask import Flask
-# # from flask_httpauth import HTTPBasicAuth
-#
-# app = Flask(__name__)
-# db = SQLAlchemy()
-# # auth = HTTPBasicAuth()
-from resources import TestController
